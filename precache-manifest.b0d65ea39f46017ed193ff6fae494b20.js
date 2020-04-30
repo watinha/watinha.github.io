93d42d281a8111fe71cd09d54c0bdd40ccfd2ae3
Willian Watanabe <watinha@gmail.com>
2020-04-30 11:32:22 -0300

    using react.js to build the homepage

---
+++ precache-manifest.b0d65ea39f46017ed193ff6fae494b20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3b57d188bb8c3198ca9588c7007827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c639a692c1aa9a2f9a",
+    "url": "/static/css/main.eeeb6c55.chunk.css"
+  },
+  {
+    "revision": "8301c2e4f8f6ff191815",
+    "url": "/static/js/2.15d3b6e4.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.15d3b6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c639a692c1aa9a2f9a",
+    "url": "/static/js/main.e7895e2a.chunk.js"
+  },
+  {
+    "revision": "33d019756279af468c7a",
+    "url": "/static/js/runtime-main.dca32d7f.js"
+  }
+]);
