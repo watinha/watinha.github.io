0ccf41cd21e6ebbaf5de6cc71379292b5f503090
Willian Watanabe <watinha@gmail.com>
2020-04-30 15:46:44 -0300

    more fixes to mobile layout

---
+++ precache-manifest.b2772baa6ece57363139e858183543b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a887a6f13685c18ee717dc127ae4fe78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eff349063867c40e08b0",
+    "url": "/static/css/main.b5aafc8a.chunk.css"
+  },
+  {
+    "revision": "8301c2e4f8f6ff191815",
+    "url": "/static/js/2.15d3b6e4.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.15d3b6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff349063867c40e08b0",
+    "url": "/static/js/main.e7895e2a.chunk.js"
+  },
+  {
+    "revision": "33d019756279af468c7a",
+    "url": "/static/js/runtime-main.dca32d7f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1246e70b70f80b98e12a8c24eab41282.js"
+  "/precache-manifest.b2772baa6ece57363139e858183543b8.js"
 );
 
 self.addEventListener('message', (event) => {
