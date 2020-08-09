fc92a27cabf04cdaea368b0d1e2c1c3cc4fd9fff
Willian Watanabe <watinha@gmail.com>
2020-08-09 00:26:35 -0300

    uploading new files

---
+++ precache-manifest.4e58c6a9c4dde11d21138a8aa6f18a5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15ce20253caa7f947c86b2ce308f283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5adafe862806617c41c5",
+    "url": "/static/css/main.89fca322.chunk.css"
+  },
+  {
+    "revision": "8301c2e4f8f6ff191815",
+    "url": "/static/js/2.15d3b6e4.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.15d3b6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5adafe862806617c41c5",
+    "url": "/static/js/main.b43fc67b.chunk.js"
+  },
+  {
+    "revision": "33d019756279af468c7a",
+    "url": "/static/js/runtime-main.dca32d7f.js"
+  }
+]);
