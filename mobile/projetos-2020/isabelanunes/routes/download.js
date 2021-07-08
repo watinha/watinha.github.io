33c9152e842f4d40403e152560eb81a37b814133
Willian Watanabe <watinha@gmail.com>
2021-07-08 11:59:58 -0300

    removing react.js projects

--- mobile/projetos-2020/isabelanunes/routes/download.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var Data = require("../model/files");
-
-router.get("/:filename", (req, res) => Data.download(req, res));
-
-module.exports = router;
