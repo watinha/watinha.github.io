916a86d5fe47ba2da908261fb23ab3dff6510efc
Willian Massami Watanabe <watinha@gmail.com>
2022-10-26 15:29:28 -0300

    Fix activity hours and simple changes to layout

--- convalidacao-matriz/index.js
+++ convalidacao-matriz/index.js
@@ -1,29 +1,25 @@
 const grade_nova = {
 	'Desenvolvimento Sem Código': { ch: 30 },
-	'Fundamentos de Programação': { ch: 90 },
+	'Fundamentos de Programação': { ch: 120 },
 	'Lógica e Introdução à Estatística': { ch: 60 },
 	'Introdução aos Sistemas Operacionais': { ch: 30 },
 	'Introdução a Análise e Desenvolvimento de Sistemas': { ch: 30 },
 	'Comunicação Oral e Escrita': { ch: 60 },
-	'Programação Orientada a Objetos': { ch: 90 },
+	'Programação Orientada a Objetos': { ch: 120 },
 	'Empreendedorismo': { ch: 30 },
-	'Banco de Dados': { ch: 60 },
+	'Banco de Dados': { ch: 90 },
 	'Inglês Instrumental': { ch: 30 },
 	'Interação Homem Computador': { ch: 30 },
 	'Programação Web Front-end': { ch: 60 },
 	'Programação Web Back-end': { ch: 60 },
-	'Estrutura de Dados': { ch: 60 },
+	'Estrutura de Dados': { ch: 90 },
 	'Desenvolvimento Ágil': { ch: 60 },
 	'Redes de Computadores': { ch: 60 },
 	'Economia': { ch: 30 },
 	'Estratégias de Inovação': { ch: 30 },
-	'Programação Web Fullstack': { ch: 60 },
-	'Arquitetura de Software': { ch: 60 },
-	'Estratégias de Programação': { ch: 60 },
-	'Disciplina Certificadora Comum': { ch: 120, r: true },
-	'Disciplina Certificadora Identitária': { ch: 120, r: true},
-	'Disciplina Certificadora Específica': { ch: 300, r: true },
-  'Estágio Curricular Obrigatório': { ch: 400, r: true },
+	'Programação Web Fullstack': { ch: 90 },
+	'Arquitetura de Software': { ch: 90 },
+	'Estratégias de Programação': { ch: 90 },
 	'Teste de Software': { ch: 60, r: true, op: true },
 	'Computação em Nuvem': { ch: 60, r: true, op: true },
 	'Programação para Dispositivos Móveis': { ch: 60, r: true, op: true },
@@ -32,53 +28,60 @@ const grade_nova = {
 	'Mineração de Dados': { ch: 60, r: true, op: true },
 	'Engenharia de Requisitos': { ch: 60, r: true, op: true },
 	'Gerenciamento de Projeto de Software': { ch: 60, r: true, op: true },
+	'Análise e Projeto Orientado a Objetos': { ch: 60, r: true, op: true },
+	'Banco de Dados 2': { ch: 60, op: true },
+	'Banco de Dados 3': { ch: 60, op: true },
+	'Programação Distribuída': { ch: 60, op: true },
+	'Segurança e Auditoria de Sistemas': { ch: 60, op: true },
+	'Disciplina Certificadora Comum': { ch: 120, r: true },
+	'Disciplina Certificadora Identitária': { ch: 120, r: true},
+	'Disciplina Certificadora Específica': { ch: 300, r: true },
+  'Estágio Curricular Obrigatório': { ch: 360, r: true },
 };
 
 const grade_antiga = {
-	'Laboratório de Informática': { ch: 60, eq: grade_nova['Desenvolvimento Sem Código'] },
-	'Algoritmos': { ch: 60, eq: grade_nova['Fundamentos de Programação'] },
-	'Fundamentos de Matemática': { ch: 60, eq: grade_nova['Lógica e Introdução à Estatística'] },
-	'Organização de Computadores': { ch: 30 },
-	'Introdução a Análise e Desenvolvimento de Sistemas': { ch: 30, eq: grade_nova['Introdução a Análise e Desenvolvimento de Sistemas'] },
-	'Comunicação Oral e Escrita': { ch: 60, eq: grade_nova['Comunicação Oral e Escrita'] },
-	'Programação Orientada a Objetos': { ch: 60, eq: grade_nova['Programação Orientada a Objetos'] },
-	'Engenharia de Software': { ch: 30, eq: grade_nova['Desenvolvimento Ágil'] },
-	'Banco de Dados 1': { ch: 60, eq: grade_nova['Banco de Dados'] },
-	'Inglês Instrumental': { ch: 30, eq: grade_nova['Inglês Instrumental'] },
-	'Estatística': { ch: 60 },
-	'Técnicas de Programação': { ch: 60, eq: grade_nova['Estratégias de Programação'] },
-	'Programação Desktop': { ch: 60, eq: grade_nova['Programação Web Front-end'] },
-	'Estrutura de Dados': { ch: 60, eq: grade_nova['Estrutura de Dados'] },
-	'Banco de Dados 2': { ch: 60 },
-	'Sistemas Operacionais': { ch: 60, eq: grade_nova['Introdução aos Sistemas Operacionais'] },
-	'Análise e Projeto Orientado a Objetos': { ch: 60 },
-	'Programação Web': { ch: 60, eq: grade_nova['Programação Web Back-end'] },
-	'Gerenciamento de Projetos de Software': { ch: 60, eq: grade_nova['Gerenciamento de Projeto de Software'] },
-	'Banco de Dados 3': { ch: 60 },
-	'Redes de Computadores': { ch: 60, eq: grade_nova['Redes de Computadores'] },
-	'Oficina de Integração': { ch: 30 },
-	'Metodologia de Pesquisa': { ch: 30 },
-	'Programação Móvel': { ch: 60, eq: grade_nova['Programação para Dispositivos Móveis'] },
-	'Arquitetura de Software': { ch: 60, eq: grade_nova['Arquitetura de Software'] },
-	'Interação Homem Computador': { ch: 60, eq: grade_nova['Interação Homem Computador'] },
-	'Programação Distribuída': { ch: 60 },
-	'Segurança e Auditoria de Sistemas': { ch: 60 },
-	'Qualidade de Software': { ch: 30 },
-	'Tópicos em Computação': { ch: 60, eq: grade_nova['Programação Web Fullstack'] },
-	'Gestão Financeira': { ch: 30, op: true },
-	'Economia': { ch: 30, eq: grade_nova['Economia'], op: true },
-	'Gestão Empresarial': { ch: 30, eq: grade_nova['Estratégias de Inovação'], op: true },
-	'Empreendedorismo': { ch: 30, eq: grade_nova['Empreendedorismo'], op: true },
+	'Laboratório de Informática': { ch: 71, eq: grade_nova['Desenvolvimento Sem Código'] },
+	'Algoritmos': { ch: 71, eq: grade_nova['Fundamentos de Programação'] },
+	'Fundamentos de Matemática': { ch: 71, eq: grade_nova['Lógica e Introdução à Estatística'] },
+	'Organização de Computadores': { ch: 35 },
+	'Introdução a Análise e Desenvolvimento de Sistemas': { ch: 35, eq: grade_nova['Introdução a Análise e Desenvolvimento de Sistemas'] },
+	'Comunicação Oral e Escrita': { ch: 71, eq: grade_nova['Comunicação Oral e Escrita'] },
+	'Programação Orientada a Objetos': { ch: 71, eq: grade_nova['Programação Orientada a Objetos'] },
+	'Engenharia de Software': { ch: 35, eq: grade_nova['Desenvolvimento Ágil'] },
+	'Banco de Dados 1': { ch: 71, eq: grade_nova['Banco de Dados'] },
+	'Inglês Instrumental': { ch: 35, eq: grade_nova['Inglês Instrumental'] },
+	'Estatística': { ch: 71 },
+	'Técnicas de Programação': { ch: 71, eq: grade_nova['Estratégias de Programação'] },
+	'Programação Desktop': { ch: 71, eq: grade_nova['Programação Web Front-end'] },
+	'Estrutura de Dados': { ch: 71, eq: grade_nova['Estrutura de Dados'] },
+	'Banco de Dados 2': { ch: 71, eq: grade_nova['Banco de Dados 2'] },
+	'Sistemas Operacionais': { ch: 71, eq: grade_nova['Introdução aos Sistemas Operacionais'] },
+	'Análise e Projeto Orientado a Objetos': { ch: 71, eq: grade_nova['Análise e Projeto Orientado a Objetos'] },
+	'Programação Web': { ch: 71, eq: grade_nova['Programação Web Back-end'] },
+	'Gerenciamento de Projetos de Software': { ch: 71, eq: grade_nova['Gerenciamento de Projeto de Software'] },
+	'Banco de Dados 3': { ch: 71, eq: grade_nova['Banco de Dados 3'] },
+	'Redes de Computadores': { ch: 71, eq: grade_nova['Redes de Computadores'] },
+	'Oficina de Integração': { ch: 68, eq: grade_nova['Disciplina Certificadora Específica'] },
+	'Metodologia de Pesquisa': { ch: 35 },
+	'Programação Móvel': { ch: 71, eq: grade_nova['Programação para Dispositivos Móveis'] },
+	'Arquitetura de Software': { ch: 71, eq: grade_nova['Arquitetura de Software'] },
+	'Interação Homem Computador': { ch: 71, eq: grade_nova['Interação Homem Computador'] },
+	'Programação Distribuída': { ch: 71, eq: grade_nova['Programação Distribuída'] },
+	'Segurança e Auditoria de Sistemas': { ch: 71, eq: grade_nova['Segurança e Auditoria de Sistemas'] },
+	'Qualidade de Software': { ch: 35 },
+	'Tópicos em Computação': { ch: 71, eq: grade_nova['Programação Web Fullstack'] },
+	'Gestão Financeira': { ch: 35, op: true },
+	'Economia': { ch: 35, eq: grade_nova['Economia'], op: true },
+	'Gestão Empresarial': { ch: 35, eq: grade_nova['Estratégias de Inovação'], op: true },
+	'Empreendedorismo': { ch: 35, eq: grade_nova['Empreendedorismo'], op: true },
   'Atividades Complementares': { ch: 180, r: true },
   'Estágio Curricular Obrigatório': { ch: 400, r: true, eq: grade_nova['Estágio Curricular Obrigatório'] }
 };
 
 const ul_grade_antiga = document.querySelector('.selecao.grade.antiga'),
-      ul_grade_nova = document.querySelector('.selecao.grade.nova'),
-      sum_grade_antiga = document.querySelector('.selecao.grade.antiga span'),
-      sum_grade_nova = document.querySelector('.selecao.grade.nova span');
+      ul_grade_nova = document.querySelector('.selecao.grade.nova');
 
-const render_sum = (grade, target) => {
+const render_sum = (grade, target, opts) => {
   let result = 0,
       ops_count = 0;
   for (let unidade in grade) {
@@ -91,6 +94,12 @@ const render_sum = (grade, target) => {
         result += dados.ch;
     }
   }
+
+  if (ops_count < 3)
+    opts.classList.remove('OK')
+  else
+    opts.classList.add('OK')
+
   result += 180 - Math.min(180, ops_count * 60);
 
   target.innerHTML = `${result}h`;
@@ -125,12 +134,12 @@ const render_grade = (grade, target) => {
             render_grade(grade_nova, ul_grade_nova);
           }
         }
-        render_sum(grade, target.parentElement.querySelector('span'));
+        render_sum(grade, target.parentElement.querySelector('span'), target.parentElement.querySelector('.optativas span'));
       });
     })();
 
     target.appendChild(li);
-    render_sum(grade, target.parentElement.querySelector('span'));
+    render_sum(grade, target.parentElement.querySelector('span'), target.parentElement.querySelector('.optativas span'));
   }
 }
 
