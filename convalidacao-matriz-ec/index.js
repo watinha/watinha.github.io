a03b8cd2e95fcf1d114ea2c136d14721d9b10c42
Willian Massami Watanabe <watinha@gmail.com>
2023-06-28 17:11:21 -0300

    matriz de convalidação de EC

---
+++ convalidacao-matriz-ec/index.js
@@ -0,0 +1,299 @@
+const grade_nova = {
+
+"Algoritmos 1": { "ch":90,"op":false,"r":false},
+"Comunicação Acadêmica": { "ch":30,"op":false,"r":false},
+"Eletricidade Básica": { "ch":60,"op":false,"r":false},
+"Introdução a Engenharia de Computação": { "ch":30,"op":false,"r":false},
+"Fundamentos de Matemática": { "ch":60,"op":false,"r":false},
+"Geometria Analitica e Álgebra Linear": { "ch":90,"op":false,"r":false},
+"Algoritmos 2": { "ch":60,"op":false,"r":false},
+"Organização de Computadores": { "ch":30,"op":false,"r":false},
+"Circuitos Digitais": { "ch":60,"op":false,"r":false},
+"Fundamentos de Ética": { "ch":30,"op":false,"r":true},
+"Qualidade de Vida 1": { "ch":30,"op":false,"r":true},
+"Cálculo Diferencial e Integral 1": { "ch":60,"op":false,"r":false},
+"Espanhol Instrumental": { "ch":30,"op_hu":true,"r":true},
+"Libras 1": { "ch":30,"op_hu":true,"r":true},
+"Libras 2": { "ch":30,"op_hu":true,"r":true},
+"Sociedade e Política": { "ch":30,"op_hu":true,"r":true},
+"Tecnologia e Sociedade": { "ch":30,"op_hu":true,"r":true},
+"História da Técnica e da Tecnologia": { "ch":30,"op_hu":true,"r":true},
+"Ciências do Ambiente": { "ch":30,"op_hu":true,"r":true},
+"A Presença Africana no Brasil": { "ch":30,"op_hu":true,"r":true},
+"Economia": { "ch":30,"op_hu":true,"r":true},
+"Estrutura de Dados 1": { "ch":60,"op":false,"r":false},
+"Leitura Instrumental em Língua Inglesa": { "ch":30,"op":false,"r":true},
+"Análise de Circuitos em Corrente Contínua": { "ch":90,"op":false,"r":false},
+"Teoria de Cinemática e Mecânica": { "ch":60,"op":false,"r":false},
+"Laboratório de Cinemática e Mecânica": { "ch":30,"op":false,"r":false},
+"Cálculo Diferencial e Integral 2": { "ch":60,"op":false,"r":false},
+"Probabilidade e Estatística": { "ch":60,"op":false,"r":false},
+"Estrutura de Dados 2": { "ch":60,"op":false,"r":false},
+"Bancos de Dados": { "ch":60,"op":false,"r":false},
+"Eletrônica Básica": { "ch":60,"op":false,"r":false},
+"Teoria de Oscilações e Ondas": { "ch":30,"op":false,"r":false},
+"Laboratório de Oscilações e Ondas": { "ch":30,"op":false,"r":false},
+"Cálculo Diferencial e Integral 3": { "ch":60,"op":false,"r":false},
+"Equações Diferenciais Ordinárias": { "ch":60,"op":false,"r":false},
+"Programação Orientada a Objetos 1": { "ch":60,"op":false,"r":false},
+"Arquitetura de Computadores": { "ch":60,"op":false,"r":false},
+"Sistemas Microcontrolados": { "ch":60,"op":false,"r":false},
+"Teoria de Eletricidade e Magnetismo": { "ch":60,"op":false,"r":false},
+"Laboratório de Eletricidade e Magnetismo": { "ch":30,"op":false,"r":false},
+"Sinais e Sistemas": { "ch":60,"op":false,"r":false},
+"Certificadora da Competência 1": { "ch":90,"op":false,"r":true},
+"Programação Orientada a Objetos 2": { "ch":60,"op":false,"r":false},
+"Sistemas Operacionais": { "ch":60,"op":false,"r":false},
+"Desenvolvimento Ágil": { "ch":60,"op":false,"r":false},
+"Comunicação Organizacional": { "ch":30,"op":false,"r":true},
+"Sistemas de Controle": { "ch":60,"op":false,"r":false},
+"Transmissão de Dados": { "ch":60,"op":false,"r":false},
+"Metodologia de Pesquisa": { "ch":60,"op":false,"r":false},
+"Certificadora da Competência 2": { "ch":90,"op":false,"r":true},
+"Inteligência Artificial": { "ch":60,"op":false,"r":true},
+"Redes de Computadores": { "ch":60,"op":false,"r":true},
+"Programação Web Front-End": { "ch":60,"op":false,"r":true},
+"Gerenciamento de Projeto de Software": { "ch":60,"op":false,"r":true},
+"Empreendedorismo": { "ch":30,"op":false,"r":true},
+"Certificadora da Competência 3": { "ch":120,"op":false,"r":true},
+"Sistemas Distribuídos": { "ch":60,"op":false,"r":true},
+"Programação Web Back-End": { "ch":60,"op":false,"r":true},
+"Programação para Dispositivos Móveis": { "ch":60,"op":false,"r":true},
+"Sistemas Embarcados": { "ch":60,"op":false,"r":true},
+"Teste de Software": { "ch":60,"op":true,"r":true},
+"Computação em Nuvem": { "ch":60,"op":true,"r":true},
+"Aprendizado de Máquina": { "ch":60,"op":true,"r":true},
+"Processamento de Imagens": { "ch":60,"op":true,"r":true},
+"Mineração de Dados": { "ch":60,"op":true,"r":true},
+"Engenharia de Requisitos": { "ch":60,"op":true,"r":true},
+"Desenvolvimento de Jogos": { "ch":60,"op":true,"r":true},
+"Realidade Virtual": { "ch":60,"op":true,"r":true},
+"Internet das Coisas": { "ch":60,"op":true,"r":true},
+"Arquitetura de Software": { "ch":60,"op":true,"r":true},
+"Lógica Reconfigurável": { "ch":60,"op":true,"r":true},
+"Introdução a Projetos de Engenharia": { "ch":120,"op":true,"r":true},
+"Fundamentos de Engenharia de Segurança do Trabalho": { "ch":30,"op":true,"r":true},
+"Expressão Gráfica": { "ch":30,"op":true,"r":true},
+"Teoria de Termodinâmica, Fluidos e Ondas": { "ch":60,"op":true,"r":true},
+"Laboratório de Termodinâmica, Fluidos e Ondas": { "ch":30,"op":true,"r":true},
+"Mecânica e Resistência dos Materiais": { "ch":60,"op":true,"r":true},
+"Fenômenos de Transporte": { "ch":60,"op":true,"r":true},
+"Química": { "ch":30,"op":true,"r":true},
+
+  'Trabalho de Conclusão de Curso 1': { ch: 30, r: true },
+  'Trabalho de Conclusão de Curso 2': { ch: 30, r: true },
+  'Estágio Curricular Obrigatório': { ch: 360, r: true },
+  'Atividades Complementares': { ch: 30, r: true },
+};
+
+const grade_antiga = {
+
+"Algor\u00edtmos 1": { "ch":90,"eq": grade_nova['Algoritmos 1']},
+"Organiza\u00e7\u00e3o de Computadores": { "ch":30,"eq": grade_nova['Organização de Computadores']},
+"F\u00edsica Geral 1": { "ch":45 },
+"F\u00edsica Experimental 1": { "ch":30 },
+"Introdu\u00e7\u00e3o \u00e0 Engenharia de Computa\u00e7\u00e3o": { "ch":30,"eq": grade_nova['Introdução a Engenharia de Computação']},
+"C\u00e1lculo Diferencial e Integral 1": { "ch":90,"eq": grade_nova['Cálculo Diferencial e Integral 1']},
+"Geometria Anal\u00edtica e \u00c0lgebra Linear": { "ch":90,"eq": grade_nova['Geometria Analitica e Álgebra Linear']},
+"INGL\u00caS INSTRUMENTAL": { "ch":30,"op_hu":true, "eq": grade_nova['Leitura Instrumental em Língua Inglesa']},
+"Ci\u00eancias Humanas 1": { "ch":30,"op_hu":true, "eq": grade_nova['Espanhol Instrumental']},
+"C\u00e1lculo Diferencial e Integral 2": { "ch":60,"eq": grade_nova['Cálculo Diferencial e Integral 2']},
+"Probabilidade e Estat\u00edstica": { "ch":60,"eq": grade_nova['Probabilidade e Estatística']},
+"Algor\u00edtmos 2": { "ch":60,"eq": grade_nova['Algoritmos 2']},
+"Desenho T\u00e9cnico": { "ch":60 },
+"F\u00edsica Geral 2": { "ch":45 },
+"F\u00edsica Experimental 2": { "ch":30 },
+"Qu\u00edmica": { "ch":60, eq: grade_nova['Química']},
+"Psicologia do Trabalho": { "ch":30,"op_hu":true, "eq": grade_nova['Sociedade e Política']},
+"Ci\u00eancias Humanas 2": { "ch":30,"op_hu":true },
+"Ci\u00eancias Humanas 3": { "ch":30,"op_hu":true, "eq": grade_nova['História da Técnica e da Tecnologia']},
+"FILOSOFIA DA CIENCIA E DA TECNOLOGIA": { "ch":30,"op_hu":true },
+"A PRESEN\u00c7A AFRICANA NO BRASIL - TECNOLOGIA E HUMANISMO": { "ch":30,"op_hu":true, "eq": grade_nova['A Presença Africana no Brasil']},
+"HISTORIA DA T\u00c9CNICA E DA TECNOLOGIA": { "ch":30,"op_hu":true, "eq": grade_nova['História da Técnica e da Tecnologia']},
+"LIDERAN\u00c7A E GERENCIAMENTO": { "ch":30,"op_hu":true},
+"Rela\u00e7\u00f5es Humanas": { "ch":30,"op_hu":true},
+"FUNDAMENTOS DA \u00c9TICA": { "ch":30,"op_hu":true, eq: grade_nova['Fundamentos de Ética']},
+"TECNOLOGIA E SOCIEDADE": { "ch":30,"op_hu":true, "eq": grade_nova['Tecnologia e Sociedade']},
+"SOCIEDADE E POLITICA NO BRASIL": { "ch":30,"op_hu":true},
+"Libras 1": { "ch":30,"op_hu":true, eq: grade_nova['Libras 1']},
+"Libras 2": { "ch":30,"op_hu":true, eq: grade_nova['Libras 2']},
+"C\u00e1lculo Diferencial e Integral 3": { "ch":60,"eq": grade_nova['Cálculo Diferencial e Integral 3']},
+"Equa\u00e7\u00f5es Diferenciais": { "ch":60,"eq": grade_nova['Equações Diferenciais Ordinárias']},
+"Estrutura de Dados": { "ch":60,"eq": grade_nova['Estrutura de Dados 1']},
+"Programa\u00e7\u00e3o Orientada a Objetos": { "ch":60,"eq": grade_nova['Programação Orientada a Objetos 1']},
+"F\u00edsica Geral 3": { "ch":45,"op":null},
+"F\u00edsica Experimental 3": { "ch":30,"op":null},
+"Mec\u00e2nica dos S\u00f3lidos": { "ch":60,"op":null},
+"M\u00e9todos de Matem\u00e1tica Aplicada": { "ch":60,"op":null},
+"An\u00e1lise de Algor\u00edtmos": { "ch":60,"eq": grade_nova['Estrutura de Dados 2']},
+"Banco de Dados 1": { "ch":60,"eq": grade_nova['Bancos de Dados']},
+"An\u00e1lise e Projeto Orientado a Objetos": { "ch":60,"eq": grade_nova['Programação Orientada a Objetos 2']},
+"An\u00e1lise de Circuitos El\u00e9tricos": { "ch":60,"eq": grade_nova['Análise de Circuitos em Corrente Contínua']},
+"Matem\u00e1tica Discreta": { "ch":60,"op":null},
+"Comunica\u00e7\u00e3o Oral e Escrita": { "ch":30,"eq": grade_nova['Comunicação Acadêmica']},
+"Teoria da Computa\u00e7\u00e3o": { "ch":60,"op":null},
+"Banco de Dados 2": { "ch":60,"op":null},
+"Sistemas de Controle": { "ch":60,"eq": grade_nova['Sistemas de Controle']},
+"Arquitetura de Computadores": { "ch":60,"eq": grade_nova['Arquitetura de Computadores']},
+"Eletr\u00f4nica Digital": { "ch":60 },
+"Transmiss\u00e3o de Dados": { "ch":60,"eq": grade_nova['Transmissão de Dados']},
+"Eletr\u00f4nica Geral 1": { "ch":60,"eq": grade_nova['Eletrônica Básica']},
+"Compiladores": { "ch":60,"op":null},
+"Engenharia de Software": { "ch":60,"eq": grade_nova['Desenvolvimento Ágil']},
+"Sistemas Operacionais": { "ch":60,"eq": grade_nova['Sistemas Operacionais']},
+"Sistemas microcontrolados": { "ch":60,"eq": grade_nova['Sistemas Microcontrolados']},
+"Ci\u00eancias do Ambiente": { "ch":30,"eq": grade_nova['Ciências do Ambiente']},
+"Eletr\u00f4nica Geral 2": { "ch":60,"op":null},
+"Oficina de Integra\u00e7\u00e3o": { "ch":30,"op":null},
+"Fen\u00f4menos de Transporte": { "ch":30,"eq": grade_nova['Fenômenos de Transporte']},
+"Empreendedorismo": { "ch":30,"eq": grade_nova['Empreendedorismo']},
+"Intera\u00e7\u00e3o Humano-Computador": { "ch":60,"eq":null},
+"Intelig\u00eancia Artificial": { "ch":60,"eq": grade_nova['Inteligência Artificial']},
+"Gerenciamento de Projeto de Software": { "ch":60,"eq": grade_nova['Gerenciamento de Projeto de Software']},
+"Redes de Computadores": { "ch":60,"eq": grade_nova['Redes de Computadores']},
+"Sistemas Embarcados": { "ch":60,"eq": grade_nova['Sistemas Embarcados']},
+"Processamento Digital de Sinais": { "ch":60},
+"REDES INDUSTRIAIS": { "ch":60,"op":"[997]"},
+"AUTOMA\u00c7\u00c3O": { "ch":60,"op":"[997]"},
+"MODELAGEM DE SISTEMAS DINAMICOS": { "ch":60,"op":"[997]"},
+"REALIDADE VIRTUAL": { "ch":60,"op":"[997]", eq: grade_nova['Realidade Virtual']},
+"Desenvolvimento de Jogos": { "ch":60,"op":"[997]", eq: grade_nova['Desenvolvimento de Jogos']},
+"Game Design": { "ch":60,"op":"[997]"},
+"MULTIMIDIA": { "ch":60,"op":"[997]"},
+"CONTROLE DE PROCESSOS": { "ch":60,"op":"[997]"},
+"ROB\u00d3TICA": { "ch":60,"op":"[997]"},
+"INSTRUMENTA\u00c7\u00c3O INDUSTRIAL": { "ch":60,"op":"[997]"},
+"SISTEMAS A EVENTOS DISCRETOS": { "ch":60,"op":"[997]"},
+"FONTES ALTERNATIVAS DE ENERGIA": { "ch":60,"op":"[997]"},
+"PROGRAMA\u00c7\u00c3O PARA APLICA\u00c7\u00d5ES COMERCIAIS": { "ch":60,"op":"[997]"},
+"QUALIDADE DE SOFTWARE": { "ch":60,"op":"[997]" },
+"ARQUITETURA DE SOFTWARE": { "ch":60,"op":"[997]", eq: grade_nova['Arquitetura de Software']},
+"TESTE E VALIDA\u00c7\u00c3O DE SOFTWARE": { "ch":60,"op":"[997]", eq: grade_nova['Teste de Software']},
+"METODOLOGIAS \u00c1GEIS DE DESENVOLVIMENTO DE SOFTWARE": { "ch":60,"op":"[997]", eq: grade_nova['Desenvolvimento Ágil']},
+"T\u00d3PICOS EM ENGENHARIA DE SOFTWARE": { "ch":60,"op":"[997]", eq: grade_nova['Computação em Nuvem']},
+"COMPUTA\u00c7\u00c3O GR\u00c1FICA": { "ch":60,"op":"[997]" },
+"PROCESSAMENTO DE IMAGENS": { "ch":60,"op":"[997]", eq: grade_nova['Processamento de Imagens']},
+"T\u00d3PICOS EM PROGRAMA\u00c7\u00c3O DE SISTEMAS": { "ch":60,"op":"[997]", eq: grade_nova['Programação Web Back-End']},
+"PROGRAMA\u00c7\u00c3O PARA DISPOSITIVOS M\u00d3VEIS": { "ch":60,"op":"[997]", eq: grade_nova['Programação para Dispositivos Móveis']},
+"Minera\u00e7\u00e3o de Dados": { "ch":60,"op":"[997]", eq: grade_nova['Mineração de Dados']},
+"Reconhecimento de Padr\u00f5es": { "ch":60,"op":"[997]", eq: grade_nova['Aprendizado de Máquina']},
+"Internet das Coisas": { "ch":60,"op":"[997]", eq: grade_nova['Internet das Coisas']},
+"Gerenciamento de Projetos para Competi\u00e7\u00e3o": { "ch":90,"op":"[997]"},
+"T\u00f3picos em Computa\u00e7\u00e3o Evolutiva": { "ch":60,"op":"[997]"},
+"Introdu\u00e7\u00e3o a Banco de Dados para Biologia": { "ch":60,"op":"[997]"},
+"Introdu\u00e7\u00e3o a Programa\u00e7\u00e3o para Bioinform\u00e1tica": { "ch":60,"op":"[997]"},
+"Experimenta\u00e7\u00e3o em Computa\u00e7\u00e3o": { "ch":60,"op":"[997]"},
+"T\u00f3picos Avan\u00e7ados em Gerenciamento de Projeto Software": { "ch":60,"op":"[997]"},
+"T\u00f3picos Avan\u00e7ados em Requisitos de Software": { "ch":60,"op":"[997]", eq: grade_nova['Engenharia de Requisitos']},
+"T\u00f3picos Avan\u00e7ados em Teste de Software": { "ch":60,"op":"[997]"},
+"T\u00f3picos Avan\u00e7ados em Processo de Produ\u00e7\u00e3o de Software": { "ch":60,"op":"[997]"},
+"Servo-acionamentos Industriais": { "ch":60,"op":"[997]"},
+"Supervis\u00e3o de Processos": { "ch":60,"op":"[997]"},
+"Economia": { "ch":30,"eq": grade_nova['Economia']},
+"Programa\u00e7\u00e3o para Web": { "ch":60,"eq": grade_nova['Programação Web Front-End']},
+"Sistemas Distribu\u00eddos": { "ch":60,"eq": grade_nova['Sistemas Distribuídos']},
+"Seguran\u00e7a e Auditoria de Sistemas": { "ch":60,"op":null},
+"Metodologia de Pesquisa": { "ch":30,"eq": grade_nova['Metodologia de Pesquisa']},
+"LOGICA RECONFIGURAVEL": { "ch":60,"op":"[997]", eq: grade_nova['Lógica Reconfigurável']},
+"T\u00f3picos Matem\u00e1ticos para Engenheiros": { "ch":45,"op":"[997]"},
+"Gerenciamento de Projetos para Competi\u00e7oes nos Projetos Pedag\u00f3gicos": { "ch":90,"op":"[997]"},
+
+  'Trabalho de Conclusão de Curso 1': { ch: 60.0, eq: grade_nova['Trabalho de Conclusão de Curso 1'] },
+  'Trabalho de Conclusão de Curso 2': { ch: 60.0, r: true, eq: grade_nova['Trabalho de Conclusão de Curso 2'] },
+  'Estágio Curricular Obrigatório': { ch: 400, r: true, eq: grade_nova['Estágio Curricular Obrigatório'] },
+  'Atividades Complementares': { ch: 180, r: true, eq: grade_nova['Atividades Complementares'] },
+}
+
+
+const ul_grade_antiga = document.querySelector('.selecao.grade.antiga'),
+      ul_grade_nova = document.querySelector('.selecao.grade.nova'),
+			GRADE_NOVA_OPTS_CH = 120,
+			GRADE_NOVA_OPTS_HU_CH = 120,
+			GRADE_ANTIGA_OPTS_CH = 180,
+			GRADE_ANTIGA_OPTS_HU_CH = 90;
+
+const render_sum = (grade, target, opts, opts_hu, ch_opt, ch_hu) => {
+  let result = 0,
+      ch_done = 0,
+      ops_count = 0,
+      ch_hu_done = 0,
+      ops_hu_count = 0;
+  for (let unidade in grade) {
+    const dados = grade[unidade];
+    if (dados.op) {
+      if (dados.realizado) {
+        ops_count++;
+        ch_done += parseInt(dados.ch);
+      }
+		} else if (dados.op_hu) {
+      if (dados.realizado) {
+        ops_hu_count++;
+        ch_hu_done += parseInt(dados.ch);
+      }
+    } else {
+      if (!dados.realizado) {
+        result += parseInt(dados.ch);
+      }
+    }
+  }
+
+  if (ch_done < ch_opt)
+    opts.classList.remove('OK')
+  else
+    opts.classList.add('OK')
+
+  if (ch_hu_done < ch_hu)
+    opts_hu.classList.remove('OK')
+  else
+    opts_hu.classList.add('OK')
+
+  result += ch_opt - Math.min(ch_opt, ch_done);
+  result += ch_hu - Math.min(ch_hu, ch_hu_done);
+
+  target.innerHTML = `${result}h`;
+}
+
+const render_grade = (grade, target, ch_opt, ch_hu) => {
+  target.innerHTML = '';
+  for (unidade in grade) {
+    const li = document.createElement('li'),
+          dados = grade[unidade];
+    li.innerHTML = `${unidade} - ${dados.ch}h ${dados.r ? '(REMOTA)' : ''} ${dados.op ? '(OPTATIVA)' : ''} ${dados.op_hu ? '(Optativa de Humanas)' : ''}`;
+    if (dados.realizado)
+      li.classList.add('realizado');
+    if (dados.op)
+      li.classList.add('optativa');
+    if (dados.op_hu)
+      li.classList.add('optativa');
+
+    (() => {
+      const cur = unidade;
+      li.addEventListener('click', function () {
+        if (!grade[cur].realizado) {
+          this.classList.add('realizado');
+          grade[cur].realizado = true;
+          if (grade[cur].eq) {
+            grade[cur].eq.realizado = true;
+            render_grade(grade_nova, ul_grade_nova, GRADE_NOVA_OPTS_CH, GRADE_NOVA_OPTS_HU_CH);
+          }
+        } else {
+          this.classList.remove('realizado');
+          grade[cur].realizado = false;
+          if (grade[cur].eq) {
+            grade[cur].eq.realizado = false;
+            render_grade(grade_nova, ul_grade_nova, GRADE_NOVA_OPTS_CH, GRADE_NOVA_OPTS_HU_CH);
+          }
+        }
+        render_sum(grade, target.parentElement.querySelector('span'), target.parentElement.querySelector('.optativas span'), target.parentElement.querySelector('.optativas.humanas span'), ch_opt, ch_hu);
+      });
+    })();
+
+    target.appendChild(li);
+    render_sum(grade, target.parentElement.querySelector('span'), target.parentElement.querySelector('.optativas span'), target.parentElement.querySelector('.optativas.humanas span'), ch_opt, ch_hu);
+  }
+}
+
+(() => {
+  render_grade(grade_antiga, ul_grade_antiga, GRADE_ANTIGA_OPTS_CH, GRADE_ANTIGA_OPTS_HU_CH);
+  render_grade(grade_nova, ul_grade_nova, GRADE_NOVA_OPTS_CH, GRADE_NOVA_OPTS_HU_CH);
+})();
